2c043eed76593254b6fcde13b1cf65c6ee40df51
Leonhard Markert <curiousleo@ymail.com>
2017-06-10 19:36:08 +0100

    Remove gendb.sh

--- gendb.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-ACTION_DIRECTORY="$(bazel info output_path)/local-fastbuild/extra_actions/capture_cc_command_action"
-EXEC_DIRECTORY="$(bazel info execution_root)"
-COMMAND_DB_FILE="$(bazel info workspace)/compile_commands.json"
-
-bazel run //:collect -- "$ACTION_DIRECTORY" "$EXEC_DIRECTORY" "$COMMAND_DB_FILE"
